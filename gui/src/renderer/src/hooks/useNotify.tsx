bba5646de3dcedb4554b885850ebbac952252774
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2024-01-18 14:10:09 -0500

    Improvement: typing in useNotify

--- gui/src/renderer/src/hooks/useNotify.tsx
+++ gui/src/renderer/src/hooks/useNotify.tsx
@@ -1,18 +1,18 @@
-import { Id, toast } from "react-toastify"
+import { Id, toast } from 'react-toastify'
 
-const notify = (promise: IDeferred['promise'], jsx?: JSX.Element): Id => {
+function notify(promise: IDeferred['promise'], jsx?: JSX.Element): Id {
   const texts = {
     loading: 'Loading',
     success: 'Success',
     error: 'Error'
   }
-  const toastId = toast.loading(
-    jsx ? jsx : texts.loading,
-    { position: 'bottom-right', closeButton: true }
-  )
+  const toastId = toast.loading(jsx ? jsx : texts.loading, {
+    position: 'bottom-right',
+    closeButton: true
+  })
 
   promise
-    .then(() =>{
+    .then(() => {
       toast.update(toastId, { render: texts.success, type: 'success', isLoading: false })
       setTimeout(() => toast.dismiss(toastId), 3000)
     })
@@ -24,6 +24,8 @@ const notify = (promise: IDeferred['promise'], jsx?: JSX.Element): Id => {
   return toastId
 }
 
-const useNotify = () => notify
+type Notify = (promise: IDeferred['promise'], jsx?: JSX.Element) => Id
+
+const useNotify = (): Notify => notify
 
 export { useNotify }
